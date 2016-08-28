2e659fa357d9b29a1d6672f19d9b2487e4d00bf4
Anthony Nabuurs <kivohin@protonmail.com>
2016-08-28 19:55:42 -0400

    Refactor the if-elsif statements with case.

--- kipu_systems_katas/dee_the_generous_tipper.rb
+++ kipu_systems_katas/dee_the_generous_tipper.rb
@@ -3,17 +3,17 @@ def calc_tip(p, r)
 
 	temp_tip = price_round / 10
 
-	if r == 1
-		final_tip = temp_tip + 1
+	# final_tip = temp_tip + 1 if r == 1
+	# final_tip = temp_tip - 1 if r == 0
+	# final_tip = (temp_tip / 2) - 1 if r == -1
 
-	elsif r == 0
-		final_tip = temp_tip - 1
-
-	else
-		final_tip = (temp_tip / 2) - 1
+	final_tip = case r
+		when 1 then temp_tip + 1
+		when 0 then temp_tip - 1
+		else (temp_tip / 2) - 1
 	end
 
-	final_tip = 0 if final_tip < 0
+	return final_tip = 0 if final_tip < 0
 	final_tip
 end
 
