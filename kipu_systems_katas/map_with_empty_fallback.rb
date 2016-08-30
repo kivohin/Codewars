526f46e0750260503277c8ade5fe758452e4a7e6
Anthony Nabuurs <kivohin@protonmail.com>
2016-08-29 23:22:20 -0400

    Reorganize kata 2 solutions.

--- kipu_systems_katas/map_with_empty_fallback.rb
+++ kipu_systems_katas/map_with_empty_fallback.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
+# First solution
+# class Array
+# 	def map_with_fallback(i, f = 0)
+#     unless self.empty?	
+# 			new_arry = self.map do |item|
+# 				i.call(item)
+# 			end
+# 			return new_arry
+# 		end
+#     f[:fallback].call if f != 0
+#   end
+# end
+
 # Better solution
 class Array
 	def map_with_fallback(i, fallback: 0)
@@ -18,16 +31,3 @@ no_items = []
 
 p items.map_with_fallback(->(i) { i + 1 })
 p no_items.map_with_fallback(->(i){}, fallback: -> { 'empty' })
-
-# First solution
-# class Array
-# 	def map_with_fallback(i, f = 0)
-#     unless self.empty?	
-# 			new_arry = self.map do |item|
-# 				i.call(item)
-# 			end
-# 			return new_arry
-# 		end
-#     f[:fallback].call if f != 0
-#   end
-# end
